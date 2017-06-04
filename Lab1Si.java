c0df26271252c022a9faf92119dcd8b92bb7a691
melissadinizg <meelissadg@gmail.com>
2017-06-03 21:38:31 -0300

    enviando programa maximo

---
+++ Lab1Si.java
@@ -0,0 +1,17 @@
+package labSi;
+
+public class Lab1Si {
+
+	public static int maximo(int[] array) {
+		int max = array[0];
+
+		for (int i : array) {
+			if (i > max) {
+				max = i;
+			}
+		}
+
+		return max;
+	}
+	
+}
